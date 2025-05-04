63d9edc8b441e07e3f89fde975c9b9ad22b2b516
Nursultan Elshatuly <sultanelshatuly@gmail.com>
2025-05-04 12:31:14 +0500

    Исправил product page 2

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,25 +1,16 @@
 import ProductDetail from "@/components/ProductDetail";
 import { stripe } from "@/lib/stripe";
-import { Metadata } from "next";
 
-type PageProps = {
-  params: {
-    id: string;
-  };
-};
-
-export default async function ProductPage({ params }: PageProps) {
+export default async function ProductPage({
+  params,
+}: {
+  params: { id: string };
+}) {
   const product = await stripe.products.retrieve(params.id, {
     expand: ["default_price"],
   });
 
   const plainProduct = JSON.parse(JSON.stringify(product));
-  return <ProductDetail product={plainProduct} />;
-}
 
-// (необязательно) SEO
-export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
-  return {
-    title: `Товар: ${params.id} | Nordique`,
-  };
+  return <ProductDetail product={plainProduct} />;
 }
